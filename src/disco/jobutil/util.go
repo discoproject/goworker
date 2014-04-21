38dffa78c1b57f2b7e682fe2fe2abd1b7a6bd6cb
Shayan Pooya <shayan@chango.com>
2014-04-21 14:15:26 -0400

    Add the jobutil package.
    This package implements some utilities that can be used by other packages.

---
+++ src/disco/jobutil/util.go
@@ -0,0 +1,96 @@
+package jobutil
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func Check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+const POLL_INTERVAL = 2000
+
+func encode(jobname string) []byte {
+	list := make([]interface{}, 2)
+	list[0] = POLL_INTERVAL
+	list[1] = []string{jobname}
+	json_jobname, err := json.Marshal(list)
+	Check(err)
+	return json_jobname
+}
+
+func decode_response(input []byte) (status string, results []string) {
+	result := make([]interface{}, 1)
+	err := json.Unmarshal(input, &result)
+	Check(err)
+	input0 := result[0].([]interface{})
+	// jobname := input0[0].(string)
+
+	result_list := input0[1].([]interface{})
+	status = result_list[0].(string)
+	inter0 := result_list[1].([]interface{})
+	if len(inter0) == 0 {
+		results = []string{""}
+	} else {
+		inter1 := inter0[0].([]interface{})
+		results = make([]string, len(inter1))
+		for i, item := range inter1 {
+			results[i] = item.(string)
+		}
+	}
+	return
+}
+
+func get_results(c chan []string, errChan chan error, url string, reader io.Reader) {
+	resp, err := http.Post(url, "application/json", reader)
+	defer resp.Body.Close()
+	// TODO only retry on certain errors
+	if err != nil || resp.StatusCode != http.StatusOK {
+		time.Sleep(time.Duration(POLL_INTERVAL) * time.Millisecond)
+		get_results(c, errChan, url, reader)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		errChan <- err
+	}
+	status, inputs := decode_response(body)
+	if status == "active" {
+		time.Sleep(time.Duration(POLL_INTERVAL) * time.Millisecond)
+		get_results(c, errChan, url, reader)
+
+	} else if status == "ready" {
+		c <- inputs
+	}
+}
+
+type TimeOut struct {
+}
+
+func (t *TimeOut) Error() string {
+	return "time out"
+}
+
+func Wait(master string, jobname string, timeout time.Duration) ([]string, error) {
+	encoded := encode(jobname)
+	url := master + "/disco/ctrl/get_results"
+	c := make(chan []string)
+	errChan := make(chan error)
+	go get_results(c, errChan, url, bytes.NewReader(encoded))
+	select {
+	case <-time.After(timeout * time.Second):
+		return nil, new(TimeOut)
+		panic("not finished yet")
+	case outputs := <-c:
+		return outputs, nil
+	case err := <-errChan:
+		return nil, err
+	}
+}
---
+++ src/disco/jobutil/util_test.go
@@ -0,0 +1,36 @@
+package jobutil
+
+import (
+	"testing"
+)
+
+func TestEncodeOne(t *testing.T) {
+	result := encode("gojob")
+	if string(result) != "[2000,[\"gojob\"]]" {
+		t.Error("bad_encoding", string(result))
+	}
+}
+
+func TestDecodeActive(t *testing.T) {
+	input := "[[\"gojob@576:17a3d:e4372\",[\"active\",[]]]]"
+	status, result := decode_response([]byte(input))
+	if status != "active" {
+		t.Error("status is not correct", status)
+	}
+
+	if result[0] != "" {
+		t.Error("should not find any input", result[0])
+	}
+}
+
+func TestDecodeReady(t *testing.T) {
+	input := "[[\"gojob@576:175ca:c4eb\",[\"ready\",[[\"disco://input\"]]]]]"
+	status, result := decode_response([]byte(input))
+	if status != "ready" {
+		t.Error("status is not correct", status)
+	}
+
+	if result[0] != "disco://input" {
+		t.Error("should not find any input", result[0])
+	}
+}
