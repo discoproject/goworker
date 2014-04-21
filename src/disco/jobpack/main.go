eb300e0c64499d1cd2d204587559bd50e57389ac
Shayan Pooya <shayan@chango.com>
2014-04-21 10:01:23 -0400

    jobpack: Fail early if there is not enough arguments to the jobpack.

--- src/disco/jobpack/main.go
+++ src/disco/jobpack/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -9,6 +10,10 @@ var workerDir string
 var jobInputs []string
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: jobpack master_url worker_dir input(s)")
+		os.Exit(1)
+	}
 	master = os.Args[1]
 	workerDir = os.Args[2]
 	jobInputs = os.Args[3:]
